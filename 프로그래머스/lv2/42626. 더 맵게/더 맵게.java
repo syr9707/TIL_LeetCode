5944ea1edbbe2a085d79a0872166b563f12134a5
YR SHIN <105832596+syr9707@users.noreply.github.com>
2023-01-02 15:54:17 +0900

    [level 2] Title: 더 맵게, Time: 1978.55 ms, Memory: 127 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42626. 더 맵게/더 맵게.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] scoville, int K) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        int answer = 0;
+        
+        for (int i = 0; i < scoville.length; i++) {
+            pq.offer(scoville[i]);
+        }
+                           
+        while (!pq.isEmpty()) {
+            int current = pq.poll();
+            
+            if (current < K) {
+                if (pq.size() == 0) {
+                    return -1;
+                }
+                int next = pq.poll();
+                int sum = current + next * 2;
+                pq.offer(sum);
+                answer++;
+                
+            }
+        }
+        return answer;
+    }
+}
