44b24fe0d270b7139733dc03f074939e4281e04a
YR SHIN <105832596+syr9707@users.noreply.github.com>
2023-03-05 15:15:57 +0900

    [level 3] Title: 최고의 집합, Time: 0.01 ms, Memory: 52.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/12938. 최고의 집합/최고의 집합.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n, int s) {
+        //int[] answer = new int[n];
+        
+        if(n > s) return new int[] {-1};
+        int[] answer = new int[n];
+        
+        for(int i = 0; i < n; i++) {
+            answer[i] = s / n;
+        }
+        
+        for(int i = 0; i < (s % n); i++) {
+            answer[i]++;
+        }
+        
+        Arrays.sort(answer);
+        
+        return answer;
+    }
+}
