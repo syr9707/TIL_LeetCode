4c3d209acd4b8c017bb3dd9a40087cf8fb35b639
YR SHIN <105832596+syr9707@users.noreply.github.com>
2023-04-10 18:17:24 +0900

    [Gold III] Title: 행렬 곱셈 순서, Time: 268 ms, Memory: 17472 KB -BaekjoonHub

---
+++ 백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		int N = Integer.parseInt(br.readLine());
+		int[][] dp = new int[N][N];
+		int[][] process = new int[N][2];
+		
+		for(int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			process[i][0] = Integer.parseInt(st.nextToken());
+			process[i][1] = Integer.parseInt(st.nextToken());
+		}
+		
+		for(int k = 1; k < N; k++) {
+			for(int i = 0; i + k < N; i++) {
+				dp[i][i+k] = Integer.MAX_VALUE;
+				for(int j = i; j < i + k; j++)
+					dp[i][i+k] = Math.min(dp[i][i+k], dp[i][j]+dp[j+1][i+k] + process[i][0]*process[j][1]*process[i+k][1]);
+			}
+		}
+		
+		System.out.println(dp[0][N-1]);
+		
+	}
+}
