e0000f0d2ad2efd40313ae2dd09a9311c2697563
YR SHIN <105832596+syr9707@users.noreply.github.com>
2023-04-10 13:36:10 +0900

    [Silver II] Title: 연속합, Time: 272 ms, Memory: 24720 KB -BaekjoonHub

---
+++ 백준/Silver/1912. 연속합/연속합.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+        String[] str = br.readLine().split(" ");
+        int[] arr = new int[N];
+        int[] dp = new int[N];
+        int max;
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(str[i]);
+        }
+        dp[0] = arr[0];
+        max = arr[0];
+        for(int i=1; i<N; i++){
+            dp[i] = Math.max(dp[i-1]+arr[i], arr[i]);
+            
+            max = Math.max(max, dp[i]);
+        }
+        
+        System.out.println(max);
+
+	}
+
+}
