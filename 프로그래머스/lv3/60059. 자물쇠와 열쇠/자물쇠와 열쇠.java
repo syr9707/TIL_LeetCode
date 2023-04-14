19368bbf0c7a46116a719f92b91c11d2f1f47271
YR SHIN <105832596+syr9707@users.noreply.github.com>
2023-04-15 08:40:56 +0900

    [level 3] Title: 자물쇠와 열쇠, Time: 0.13 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/lv3/60059. 자물쇠와 열쇠/자물쇠와 열쇠.java
+++ 프로그래머스/lv3/60059. 자물쇠와 열쇠/자물쇠와 열쇠.java
@@ -2,47 +2,92 @@ import java.util.*;
 
 class Solution {
     public boolean solution(int[][] key, int[][] lock) {
-        int point = key.length - 1;     
-        for(int x = 0; x < point + lock.length; x++) { 
-            for(int y = 0; y < point + lock.length; y++) {
-                for(int r = 0; r < 4; r++) {
-                    int[][] newLock = new int[lock.length + key.length * 2][lock.length + key.length * 2]; 
-                    for(int i = 0; i < lock.length; i++) {
-                        for(int j = 0; j < lock.length; j++) {
-                            newLock[i + point][j + point] = lock[i][j];
-                        }
-                    }
-                    match(newLock, key, r, x, y);
-                    if(check(newLock, point, lock.length)) return true;
-                }
+        
+        
+        boolean answer = true;
+        
+        int m = key.length;
+        int n = lock.length;
+        
+        int len = n + (m * 2) - 2;
+        int[][] map = new int[len][len]; // 확장시킨 배열
+        
+        // 확장시킨 배열에 Lock 표시
+        for(int i = m - 1; i < m + n - 1; i++) {
+            for(int j = m - 1; j < m + n - 1; j++) {
+                map[i][j] = lock[i - (m - 1)][j - (m - 1)];
             }
         }
+        
+        // 4번 조사
+        for(int i=0; i<4; i++){
+            if(check(map, key, n)){
+                return true;
+            }
+            rotate(key); // 시계방향 90도 회전
+        }
+        
+        
         return false;
     }
     
-    public void match(int[][] newLock, int[][] key, int rot, int x, int y) {
-        int len = key.length;
-        for(int i = 0; i < len; i++) {
-            for(int j = 0; j < len; j++) {
-                if(rot == 0) {
-                    newLock[x + i][y + j] += key[i][j];
-                } else if(rot == 1) { 
-                    newLock[x + i][y + j] += key[j][len - i - 1];
-                } else if(rot == 2) {
-                    newLock[x + i][y + j] += key[len - i - 1][len- j - 1];
-                } else { 
-                    newLock[x + i][y + j] += key[len - j - 1][i];
+    /* 키가 자물쇠에 맞는지 체크 */
+    public static boolean check(int[][] map, int[][] key, int lockLen){
+        int keyLen = key.length;
+        int mapLen = map.length;
+        for(int i=0; i<mapLen-keyLen+1; i++){
+            for(int j=0; j<mapLen-keyLen+1; j++){
+                
+                /* map에 key를 더함 */
+                for(int k=0; k<keyLen; k++){
+                    for(int l=0; l<keyLen; l++){
+                        map[i+k][j+l] += key[k][l];
+                    }
                 }
+                
+                /* 자물쇠 부분이 전부 1이 됐는지 확인 */
+                boolean flag = true;
+                for(int k=keyLen-1; k<keyLen+lockLen-1; k++){
+                    for(int l=keyLen-1; l<keyLen+lockLen-1; l++){
+                        if(map[k][l] != 1){ // 1이 아니면 홈이 안 맞는 것임
+                            flag = false;
+                            break;
+                        }
+                    }
+                    if(!flag) break;
+                }
+                
+                if(flag) return true;   // 전부 1이였다면 true
+                
+                /* map을 원상 복귀 시킴 -> map에서 key를 뺌 */
+                for(int k=0; k<keyLen; k++){
+                    for(int l=0; l<keyLen; l++){
+                        map[i+k][j+l] -= key[k][l];
+                    }
+                }
+                
             }
         }
+        
+        return false;
     }
     
-    public boolean check(int[][] newLock, int point, int len) {
-        for(int i = 0; i < len; i++) {
-            for(int j = 0; j < len; j++) {
-                if(newLock[point + i][point + j] != 1) return false;
+    /* key 시계 방향 90도 회전 */
+    public static void rotate(int[][] key){
+        int len = key.length;
+        int[][] temp = new int[len][len];
+        
+        for(int i=0; i<len; i++){
+            for(int j=0; j<len; j++){
+                temp[i][j] = key[len-j-1][i];
             }
         }
-        return true;
+          
+        for(int i=0; i<len; i++){
+            for(int j=0; j<len; j++){
+                key[i][j] = temp[i][j];
+            }
+        }
+        
     }
 }
