5fd9bbbd591f6109224293836c486e9db3aab47c
YR SHIN <105832596+syr9707@users.noreply.github.com>
2023-04-14 17:14:51 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 6.73 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/lv2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/lv2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -2,60 +2,56 @@ import java.util.*;
 
 class Solution {
     
-    static int[][] arr;
+    static int[][] map;
     
     public int solution(int n, int[][] wires) {
         int answer = n;
-        arr= new int[n+1][n+1];
         
-        //1. 인접행렬에 input
-        for(int i=0; i<wires.length; i++){
-            arr[wires[i][0]][wires[i][1]]=1;
-            arr[wires[i][1]][wires[i][0]]=1;
+        map = new int[n + 1][n + 1];
+        for(int i = 0; i < wires.length; i++) {
+            int a = wires[i][0];
+            int b = wires[i][1];
+            
+            map[a][b] = 1;
+            map[b][a] = 1;
         }
         
-        //2. 선을 하나씩 끊어보며 순회
-        int a, b;
-        for(int i=0; i<wires.length; i++) {
-            a= wires[i][0];
-            b= wires[i][1];
+        for(int i = 0; i < wires.length; i++) {
+            int a = wires[i][0];
+            int b = wires[i][1];
             
-            //선을 하나 끊고
-            arr[a][b]=0;
-            arr[b][a]=0;
+            map[a][b] = 0;
+            map[b][a] = 0;
             
-            //bfs
-            answer= Math.min(answer, bfs(n, a));
+            answer = Math.min(answer, bfs(n, a));
             
-            //선 다시 복구
-            arr[a][b]=1;
-            arr[b][a]=1;
+            map[a][b] = 1;
+            map[b][a] = 1;
         }
         
         return answer;
     }
     
-    public int bfs(int n, int start) {
-        int[] visit= new int[n+1];
-        int cnt=1;
+    public static int bfs(int n, int start) {
+        int count = 1;
+        Queue<Integer> q = new LinkedList<>();
+        boolean[] check = new boolean[n + 1];
         
-        Queue<Integer> queue= new LinkedList<>();
-        queue.offer(start);
+        q.add(start);
+        check[start] = true;
         
-        while(!queue.isEmpty()){
-            int point= queue.poll();
-            visit[point]= 1;
+        while(!q.isEmpty()) {
+            int now = q.poll();
             
-            for(int i=1; i<=n; i++){ //point와 연결된 애들 중에 방문한적 없는 노드 전부 큐에 넣기
-                if(visit[i]==1) continue;
-                if(arr[point][i]==1) {
-                    queue.offer(i);
-                    cnt++;
+            for(int i = 1; i <= n; i++) {
+                if(map[now][i] == 1 && !check[i]) {
+                    q.add(i);
+                    check[i] = true;
+                    count++;
                 }
             }
         }
         
-        return (int)Math.abs(n-2*cnt); //cnt-(n-cnt);
+        return Math.abs(2 * count - n);
     }
-    
 }
