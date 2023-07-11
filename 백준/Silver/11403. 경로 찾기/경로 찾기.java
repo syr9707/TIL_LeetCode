167f78521d1020d220b8ca786978fa778d6c7ea5
YR SHIN <105832596+syr9707@users.noreply.github.com>
2023-07-11 17:41:58 +0900

    [Silver I] Title: 경로 찾기, Time: 372 ms, Memory: 19400 KB -BaekjoonHub

---
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		int N = Integer.parseInt(br.readLine());
+		int[][] arr = new int[N][N];
+		
+		for(int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j = 0; j < N; j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		for(int k = 0; k < N; k++) {
+			for(int i = 0; i < N; i++) {
+				for(int j = 0; j < N; j++) {
+					if(arr[i][k] == 1 && arr[k][j] == 1) {
+						arr[i][j] = 1;
+					}
+				}
+			}
+		}
+		
+		for(int i = 0; i < N; i++) {
+			for(int j = 0; j < N; j++) {
+				System.out.print(arr[i][j] + " ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
