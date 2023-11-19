61b341ee742bf37c81d4e240199fc4aa0aaa2567
YR SHIN <105832596+syr9707@users.noreply.github.com>
2023-11-19 12:52:15 +0900

    [Silver I] Title: 카드 구매하기, Time: 160 ms, Memory: 14496 KB -BaekjoonHub

---
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[n + 1];
+        int[] dp = new int[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                dp[i] = Math.max(dp[i], arr[j] + dp[i - j]);
+            }
+        }
+
+        System.out.println(dp[n]);
+    }
+}
